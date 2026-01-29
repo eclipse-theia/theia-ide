3d71ce76b855045a9f931dd6891e4176be3195a5
Nina Doschek <ndoschek@eclipsesource.com>
2026-01-29 11:22:16 +0100

    chore: update built-in extensions and java extensions
    Resolves GH-580
    
    - fix test case for typescript builtin

--- applications/electron/test/app.spec.js
+++ applications/electron/test/app.spec.js
@@ -231,6 +231,6 @@ describe('Theia App', function () {
 
     // Exemplary check a few extensions
     expect(extensionNames).to.include('Debugger for Java');
-    expect(extensionNames).to.include('TypeScript Language Basics (built-in)');
+    expect(extensionNames).to.include('TypeScript and JavaScript Language Features (built-in)');
   });
 });
